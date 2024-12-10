1bb6a9928d502efa0472904410e7321a5efffa53
Craig Ambrose <craig@craigambrose.com>
2024-12-11 10:13:44 +1100

    send hostname

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use anyhow::{bail, Result};
+use gethostname::gethostname;
 use p2panda_core::{Hash, PrivateKey, PublicKey, Signature};
 use p2panda_discovery::mdns::LocalDiscovery;
 use p2panda_net::network::{FromNetwork, ToNetwork};
-use p2panda_net::{NetworkBuilder, TopicId};
+use p2panda_net::{NetworkBuilder, NetworkId, TopicId};
 use p2panda_sync::TopicQuery;
 use rand::random;
 use serde::{Deserialize, Serialize};
@@ -27,8 +28,12 @@ impl TopicId for ChatTopic {
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let network_id = [0; 32];
-    let topic = ChatTopic::new("my_chat");
+    let site_name = get_site_name();
+
+    let network_slug = "merri-bek.tech";
+    let network_id: NetworkId = Hash::new(network_slug).into();
+
+    let topic = ChatTopic::new("site_management");
 
     let private_key = PrivateKey::new();
 
@@ -45,7 +50,7 @@ async fn main() -> Result<()> {
                 FromNetwork::GossipMessage { bytes, .. } => {
                     match Message::decode_and_verify(&bytes) {
                         Ok(message) => {
-                            print!("{}: {}", message.public_key, message.text);
+                            println!("Received {}: {}", message.public_key, message.text);
                         }
                         Err(err) => {
                             eprintln!("invalid gossip message: {err}");
@@ -59,7 +64,10 @@ async fn main() -> Result<()> {
 
     println!(".. waiting for peers to join ..");
     let _ = ready.await;
+
     println!("found other peers, you're ready to chat!");
+
+    announce_site(&private_key, &site_name, &tx).await?;
 
     let (line_tx, mut line_rx) = mpsc::channel(1);
     std::thread::spawn(move || input_loop(line_tx));
@@ -128,4 +136,26 @@ fn input_loop(line_tx: mpsc::Sender<String>) -> Result<()> {
         line_tx.blocking_send(buffer.clone())?;
         buffer.clear();
     }
+}
+
+fn get_site_name() -> String {
+    let hostname = gethostname();
+    return hostname.to_string_lossy().to_string();
+}
+
+async fn announce_site(
+    private_key: &PrivateKey,
+    site_name: &str,
+    tx: &tokio::sync::mpsc::Sender<ToNetwork>,
+) -> Result<()> {
+    println!("Announcing myself: {}", site_name);
+    tx.send(ToNetwork::Message {
+        bytes: Message::sign_and_encode(
+            &private_key,
+            &format!("{} has joined the chat", site_name),
+        )?,
+    })
+    .await
+    .ok();
+    Ok(())
 }
